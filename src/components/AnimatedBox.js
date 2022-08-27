914dc5861dbed3d7f6ba125ac22ffa556f3206c0
chhedadhruv <96813681+chhedadhruv@users.noreply.github.com>
2022-08-27 12:26:27 +0530

    main

---
+++ src/components/AnimatedBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/Animation.css'
+
+function AnimatedBox() {
+  return (
+    <div className='box'>
+        <div className='title'>
+            <h1 className='line-1 anim-typewriter'>DISCRETE STRUCTURES</h1>
+            <h1 className='line-1 anim-typewriter'>AND</h1>
+            <h1 className='line-1 anim-typewriter'>GRAPH THEORY</h1>
+            <h1 className='line-1 anim-typewriter'>VIRTUAL LAB</h1>
+        </div>
+    </div>
+  )
+}
+
+export default AnimatedBox
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import dsgt from '../assets/DSGT.png'
+import '../styles/Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <nav>
+            <div className='logo'>
+                <img src={logo} alt='logo' />
+            </div>
+            <div className='links'>
+                <img src={dsgt} alt='logo' />
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
