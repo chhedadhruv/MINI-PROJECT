b3f8268542f525985b6fc115c657efd68fbe934a
chhedadhruv <96813681+chhedadhruv@users.noreply.github.com>
2022-09-11 23:05:59 +0530

    created button and ui improvement

---
+++ src/Pages/ExperimentList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function ExperimentList() {
+  return (
+    <div>ExperimentList</div>
+  )
+}
+
+export default ExperimentList
--- src/components/AnimatedBox.js
+++ src/components/AnimatedBox.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../styles/Animation.css'
+import ExperimentList from '../Pages/ExperimentList'
 
 function AnimatedBox() {
   return (
+    <>
+    <div className='col'>
     <div className='box'>
       <div className='title'>
         <h1 className='line-1 anim-typewriter'>DISCRETE STRUCTURES</h1>
@@ -11,6 +14,11 @@ function AnimatedBox() {
         <h1 className='line-1 anim-typewriter'>VIRTUAL LAB</h1>
       </div>
     </div>
+    <div className='btn'> 
+      <button>Get Started</button>
+    </div>
+    </div>
+    </>
   )
 }
 
