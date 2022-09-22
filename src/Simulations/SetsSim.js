c1d89e8b829f68a168f64a8fc42c3b8333fe999c
chhedadhruv <96813681+chhedadhruv@users.noreply.github.com>
2022-09-22 16:19:51 +0530

    1

--- src/Simulations/SetsSim.js
+++ src/Simulations/SetsSim.js
@@ -1,75 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
-import * as set from 'node-set-theory';
-import { intersect, union } from 'node-set-theory'
+// import * as set from 'node-set-theory';
+import { union } from 'node-set-theory'
+import Zet from 'zet';
 
 function SetsSim() {
-    // const setAElem1 = document.getElementById("input-seta1").value;
-    // const setAElem2 = document.getElementById("input-seta2").value;
-    // const setAElem3 = document.getElementById("input-seta3").value;
-    // const setAElem4 = document.getElementById("input-seta4").value;
-    // const setAElem5 = document.getElementById("input-seta5").value;
-    // const setBElem1 = document.getElementById("input-setb1").value;
-    // const setBElem2 = document.getElementById("input-setb2").value;
-    // const setBElem3 = document.getElementById("input-setb3").value;
-    // const setBElem4 = document.getElementById("input-setb4").value;
-    // const setBElem5 = document.getElementById("input-setb5").value;
-    const a = union([1, 2], [3, 2]);
-    console.log(a);
+    const [setAValue, setSetAValue] = useState('');
+    const [setBValue, setSetBValue] = useState('');
+    const [setUnionValue, setSetUnionValue] = useState('');
+    const handleSetAValue = (event) => {
+        setSetAValue(event.target.value);
+        // alert(event.target.value);
+    };
+    const handleSetBValue = (event) => {
+        setSetBValue(event.target.value);
+        // alert(event.target.value);
+    };
+    const handleUnion = () => {
+        // setSetUnionValue(event.value);
+        setUnionValue = union([1,2,3], [2,3,4]);
+    }
+    const a = new Zet(setAValue);
+    const b = new Zet(setBValue);
+    const c = Zet.union(a, b);
+    // console.log(c);
+    const onClickBtn = () => {
+        // const c = union([setAValue, setBValue]);
+    alert(c);
+    }
   return (
     <>
     <Navbar />
     <div className='simulation'>
         <div className='seta'>
         <div className='row'>
-            Enter 1st element of set A:
-            <input type="number" name='set-a' id='input-seta1'></input>
+            Enter elements of set A:
+            <input type="text" name='set-a' id='input-seta' onChange={ handleSetAValue } value={setAValue}></input>
         </div>
-        <div className='row'>
-            Enter 2nd element of set A:
-            <input type="number" name='set-a' id='input-seta2'></input>
-        </div>
-        <div className="row">
-            Enter 3rd element of set A:
-            <input type="number" name='set-a' id='input-seta3'></input>
-        </div>
-        <div className='row'>
-            Enter 4th element of set A:
-            <input type="number" name='set-a' id='input-seta4'></input>
-        </div>
-        <div className='row'>
-            Enter 5th element of set A:
-            <input type="number" name='set-a' id='input-seta5'></input>
-        </div>
-        <button>Submit</button>
-        Set A:-
-        <input type="number" name='set-a' value="" id='input-seta-final'></input>
         </div>
         <div className='setb'>
         <div className='row'>
-            Enter 1st element of set B:
-            <input type="number" name='set-b' id='input-setb1'></input>
+            Enter elements of set B:
+            <input type="text" name='set-b' id='input-setb' onChange={handleSetBValue} value={setBValue}></input>
         </div>
+        </div>
+        <div className='setunion'>
         <div className='row'>
-            Enter 2nd element of set B:
-            <input type="number" name='set-b' id='input-setb2'></input>
+            AUB=
+            <input type="number" name='set-union' id='input-setunion'></input>
         </div>
-        <div className="row">
-            Enter 3rd element of set B:
-            <input type="number" name='set-b' id='input-setb3'></input>
         </div>
-        <div className='row'>
-            Enter 4th element of set B:
-            <input type="number" name='set-b' id='input-setb4'></input>
-        </div>
-        <div className='row'>
-            Enter 5th element of set B:
-            <input type="number" name='set-b' id='input-setb5'></input>
-        </div>
-        <button>Submit</button>
-        Set B:-
-        <input type="number" name='set-b' value="" id='input-setb-final'></input>
-        </div>
+        <button type='submit' onClick={onClickBtn}>Submit</button>
     </div>
     </>
   )
